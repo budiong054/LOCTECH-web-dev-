46b8c84ae682d6939bd28ff7a917e6428ab75cc1
Blessing Godwin Udiong <82527332+budiong054@users.noreply.github.com>
2022-04-17 22:32:40 +0100

    media query done

--- Loan Calculator/index.js
+++ Loan Calculator/index.js
@@ -1,38 +1,3 @@
-// const btn = document.querySelector('.btn');
-// const total = document.querySelector('#total');
-//     calculate = () => { // Get the user's input from the form. Assume it is all valid. // Convert interest from a percentage to a decimal, and convert from // an annual rate to a monthly rate. Convert payment period in years // to the number of monthly payments. 
-    
-//     const principal = document.loandata.amount.value; 
-//     const interest = document.loandata.rate.value / 100 / 12; 
-//     const payments = document.loandata.years.value * 12; 
-    
-//     // Now compute the monthly payment figure, using esoteric math. 
-    
-//     const x = Math.pow(1 + interest, payments); const monthly = (principal*x*interest)/(x-1); 
-//     // Check that the result is a finite number. If so, display the results. 
-    
-//     if (!isNaN(monthly) && (monthly != Number.POSITIVE_INFINITY) && (monthly != Number.NEGATIVE_INFINITY)) { 
-//     document.loandata.payment.value = round(monthly); 
-//     document.loandata.total.value = round(monthly * payments); 
-//     document.loandata.totalinterest.value = round((monthly * payments) - principal); 
-//     } 
-//     // Otherwise, the user's input was probably invalid, so don't // display anything. 
-    
-//     else { 
-//     document.loandata.payment.value = ""; document.loandata.total.value = ""; 
-//     document.loandata.totalinterest.value = ""; 
-//     } 
-//     } 
-//     // This simple method rounds a number to two decimal places. 
-    
-//     function round(x) { 
-//     return Math.round(x*100)/100; 
-//     }
-//     btn.addEventListener('click', calculate)
-//     window.addEventListener('onchange', calculate)
-
-
-
 const btn = document.querySelector('.btn'),
 principal = document.querySelector('#principal'),
 interestRate = document.querySelector('#interest'),
@@ -42,36 +7,66 @@ totalPayment = document.querySelector('#total-payment'),
 totalInterest = document.querySelector('#total-interest');
 
 
+const valueCheck = ()=>{
+    // if(principalValue && interestRateValue && periodValue){
+    //     console.log('yes oo')
+    // }
+    // else{
+    //     console.log('No oo')
+    // }
+     
+    if(!principalValue){
+        // principal.classList.replace('output','error')
+        principal.style.
+        defaultValue()
 
+    }
+    else if(!interestRateValue){
+        console.log('yes oo')
+        defaultValue()
+    }
+    else if(!periodValue){
+        console.log('yes oo')
+        defaultValue()
+    }
+    else{
+        console.log('No oo')
+        amort()
+    }
+}
+const defaultValue = ()=>{
+    monthlyPayment.innerHTML = `N 0.00` 
+    totalPayment.innerHTML = `N 0.00`
+    totalInterest.innerHTML = `N 0.00`
+}
 
 const amort = ()=>{
     month = (principalValue*monthlyRate*(1+monthlyRate)**numberOfMonth)/(((1+monthlyRate)**numberOfMonth)-1)
 
     totalM = month*numberOfMonth
     totalI = totalM - principalValue
-}
-
-const compute = ()=>{
-    principalValue = parseInt(principal.value)
-    interestRateValue = parseInt(interestRate.value)
-    periodValue = parseInt(period.value)
-    monthlyRate = interestRateValue/(12*100)
-    numberOfMonth = periodValue*12
-
-    amort()
 
     monthlyPayment.innerHTML = `N ${Math.round(month * 100)/100}` 
     totalPayment.innerHTML = `N ${Math.round(totalM*100)/100}`
     totalInterest.innerHTML = `N ${Math.round(totalI * 100)/100}`
+}
 
-    
-    console.log(monthlyRate)
-    console.log(numberOfMonth)
+const compute = ()=>{
+    principalValue = parseFloat(principal.value)
+    interestRateValue = parseFloat(interestRate.value)
+    periodValue = parseFloat(period.value)
+    monthlyRate = interestRateValue/(12*100)
+    numberOfMonth = periodValue*12
+
+   
+
+   
+
+    valueCheck()
+
     console.log(month)
-    console.log(Math.round(month * 100)/100)
     console.log(totalM)
     console.log(totalI)
-    console.log(Math.round(totalI * 100)/100)
 }
 
 
